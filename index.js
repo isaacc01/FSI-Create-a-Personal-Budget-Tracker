aed4627e9d78513b81443441df68911d5254b888
Isaac Hernandez <Isaac@Isaacs-MacBook-Air.local>
2022-07-19 20:07:06 -0700

    Budget Example

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+console.log(weeklyExpenseQuestions)
+
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + response
+  
+}
+
+
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + response
+    
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + response
+    
+}
